c57153df48d45c45f349d1afadac3b42f65c98f0
Oleg Volkov <olegv@parallels.com>
2018-10-07 21:12:15 +0300

    Comments

--- FX2LP/VCPLoop/fw.c
+++ FX2LP/VCPLoop/fw.c
@@ -200,6 +200,15 @@ void StallEP0(void)
 #define GET_LINE_CODING (0x21)
 #define SET_CONTROL_STATE (0x22)
 
+/*
+Line coding structure:
+Offset Field       Size Value   Description
+0      dwDTERate   4    Number  Data terminal rate, in bits per second.
+4      bCharFormat 1    Number  Stop bits: 0 - 1 Stop bit, 1 - 1.5 Stop bits, 2 - 2 Stop bits
+5      bParityType 1    Number  Parity: 0 - None, 1 - Odd, 2 - Even, 3 - Mark, 4 - Space
+6      bDataBits   1    Number  Data bits (5, 6, 7, 8 or 16)
+*/
+
 // 2400 baud, no parity, 1 stop bit
 BYTE xdata LineCode[7] = {0x60,0x09,0x00,0x00,0x00,0x00,0x08};
 
@@ -231,6 +240,13 @@ void SetupCommand(void)
          break;
 
       case SET_CONTROL_STATE:
+/*
+Control Signal Bitmap Values:
+Bit position Description
+15..2        RESERVED
+1            RTS
+0            DTR
+*/
          break;
 
       case SC_GET_DESCRIPTOR:                  // *** Get Descriptor
@@ -243,7 +259,7 @@ void SetupCommand(void)
                   SUDPTRL = LSB(pDeviceDscr);
                   break;
                case GD_DEVICE_QUALIFIER:            // Device Qualifier
-                  // only retuen a device qualifier if this is a high speed
+                  // only return a device qualifier if this is a high speed
                   // capable chip.
                   if (HighSpeedCapable())
                   {
