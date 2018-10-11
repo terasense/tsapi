9d4600d184913f2bbf7cc111b0959739f0c77008
Oleg Volkov <olegv@parallels.com>
2018-10-11 09:09:05 +0300

    Cosmetic changes in test code

--- py/test/reconnect.py
+++ py/test/reconnect.py
@@ -7,6 +7,7 @@ if os.name == 'nt':
 	import dev_tools
 
 TIMEOUT = 1
+MAX_DELAY = 10
 PORT = sys.argv[1]
 dev = None
 
@@ -21,10 +22,20 @@ def send_req(com, req):
 		return False
 	try:
 		resp = com.read(ts_usb.PKT_LEN)
+		if len(resp) != ts_usb.PKT_LEN:
+			print >> sys.stderr, '\nread', len(resp), 'bytes'
+			return False
 		if not ts_usb.chk_cmd_response(resp, req):
+			print >> sys.stderr, '\nbad response'
 			return False
 		r = ts_usb.cmd_deserialize(resp)
-		return r.err == 0 and (r.status & ts_usb.ERR_ANY) == 0
+		if r.err != 0:
+			print >> sys.stderr, '\nerr', r.err
+			return False
+		if (r.status & ts_usb.ERR_ANY) != 0:
+			print >> sys.stderr, '\nstatus', r.status
+			return False
+		return True
 	except:
 		print >> sys.stderr, '\nread failed'
 		return False
@@ -52,7 +63,7 @@ def reconnect_port(com):
 				com.read(w)
 			return com
 		except:
-			delay = min(TIMEOUT, delay*2)
+			delay = min(MAX_DELAY, delay*2)
 			print >> sys.stderr, 'reopen failed'
 
 com = open_port()
