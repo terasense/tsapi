665b3684fba87aa88effb627918c9a8a443e9bc9
Oleg Volkov <olegv142@gmail.com>
2023-10-15 21:27:59 +0300

    Print data rate on test completion

--- usb/test/host/stream_read.py
+++ usb/test/host/stream_read.py
@@ -1,19 +1,20 @@
 import sys
 import serial
+import time
 
 last_sn = None
 abort_on_error = True
 
 def validate_sn(sn):
 	global last_sn
-	#print('{0:b}'.format(sn))
 	if last_sn is not None and sn != (last_sn + 1) & 0xffff:
 		if abort_on_error:
 			print('bad sequence: {0:b}, {1:b}'.format(last_sn, sn))
-			sys.exit(255)
+			return False
 		else:
 			print('!', end='', flush=True)
 	last_sn = sn
+	return True
 
 def validate(chunk):
 	n, j = 0, 0
@@ -24,22 +25,32 @@ def validate(chunk):
 		n |= ((b & 1) << j)
 		j += 1
 		if j >= 16:
-			validate_sn(n)
+			if not validate_sn(n):
+				return False
 			n, j = 0, 0
-
+	return True
 
 def run_test(port):
+	start = time.time()
+	total = 0
 	com = serial.Serial(port)
-	last_buff = None
-	while True:
-		chunk = com.read(1024)
-		if last_buff:
-			chunk, last_buff = last_buff + chunk, None
-		tail = len(chunk) % 32
-		if tail:
-			chunk, last_buff = chunk[:-tail], chunk[-tail:]
-		print('.', end='', flush=True)
-		validate(chunk)
+	try:
+		last_buff = None
+		while True:
+			chunk = com.read(1024)
+			if last_buff:
+				chunk, last_buff = last_buff + chunk, None
+			tail = len(chunk) % 32
+			if tail:
+				chunk, last_buff = chunk[:-tail], chunk[-tail:]
+			print('.', end='', flush=True)
+			total += len(chunk)
+			if not validate(chunk):
+				break
+	except KeyboardInterrupt:
+		pass
+	print('\n%f MB/sec' % (total / (1e6*(time.time() - start))))
+
 
 if __name__ == '__main__':
 	run_test(sys.argv[1])
